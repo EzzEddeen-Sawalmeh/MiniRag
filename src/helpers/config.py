273f3ef562aa4b9efba951c5b799160a4e4ab40a
EzzEddeen-Sawalmeh <ezzsawalmeh101@gmail.com>
2025-08-30 03:39:44 +0300

    start the logic of uploading files

--- src/helpers/config.py
+++ src/helpers/config.py
@@ -4,6 +4,8 @@ class Settings(BaseSettings):
     APP_NAME: str
     APP_VERSION: str
     OPENAI_API_KEY: str
+    FILE_ALLOWED_TYPES : list
+    FILE_MAX_SIZE : int
 
     model_config = SettingsConfigDict(env_file=".env")
 def get_settings():
